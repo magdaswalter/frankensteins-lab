f007d8fd3e732d0e49299a8ba97f236a8983fbed
Walter Magdas <magdaswalter@gmail.com>
2024-01-09 00:59:55 +0100

    Image size keeping

--- src/pages/collectionGenerator/ImageCombiner.tsx
+++ src/pages/collectionGenerator/ImageCombiner.tsx
@@ -5,6 +5,7 @@ const combineImages = (
   numOfImages: number
 ): Promise<string[]> => {
   return new Promise(async (resolve, reject) => {
+    // Organize files into a structured format
     const organizedFiles: {
       [key: string]: FileWithPath[];
     } = {};
@@ -20,6 +21,7 @@ const combineImages = (
       }
     });
 
+    // Function to load an image
     const loadImage = (file: FileWithPath): Promise<HTMLImageElement> =>
       new Promise((resolve) => {
         const img = new Image();
@@ -27,32 +29,41 @@ const combineImages = (
         img.src = URL.createObjectURL(file);
       });
 
+    // Preparing for combinations
     const combinationsInput = Object.values(organizedFiles);
     const combinedImages: string[] = [];
 
+    // Function to generate combinations
     const generateCombination = async (
       indexArray: number[],
       maxIndices: number[]
     ): Promise<void> => {
-      if (combinedImages.length >= numOfImages) {
-        return; // Stop generating more combinations when the limit is reached
-      }
+      if (combinedImages.length >= numOfImages) return;
 
       const combination: FileWithPath[] = indexArray.map(
         (index, i) => combinationsInput[i][index]
       );
+      const images = await Promise.all(combination.map(loadImage));
+
+      // Determine canvas size based on the largest image
+      const canvasWidth = Math.max(...images.map((img) => img.width));
+      const canvasHeight = Math.max(...images.map((img) => img.height));
 
       const canvas = document.createElement("canvas");
-      canvas.width = 800;
-      canvas.height = 600;
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
       const context = canvas.getContext("2d");
 
-      for (const file of combination) {
-        const image = await loadImage(file);
-        context?.drawImage(image, 0, 0, canvas.width, canvas.height);
-      }
+      if (context) {
+        images.forEach((img) => {
+          context.drawImage(img, 0, 0, img.width, img.height);
+        });
 
-      combinedImages.push(canvas.toDataURL());
+        combinedImages.push(canvas.toDataURL());
+      } else {
+        reject(new Error("Failed to get canvas context"));
+        return;
+      }
 
       // Increment the rightmost index and carry over if needed
       for (let i = indexArray.length - 1; i >= 0; i--) {
@@ -65,6 +76,7 @@ const combineImages = (
       }
     };
 
+    // Initiating combination generation
     const maxIndices = combinationsInput.map((files) => files.length);
     await generateCombination(
       new Array(combinationsInput.length).fill(0),
